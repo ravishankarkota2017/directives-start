dcad29259b0692874046837cf1011adc040c86a6
Ravi Shankar Kota <ravishankarkota@gmail.com>
2018-09-07 16:08:34 +0530

    using host binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BetterHighlightDirective } from './directives/better-highlight.directive';
+import { MoreBetterHighlightDirective } from './directives/more-better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    MoreBetterHighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directives/more-better-highlight.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, HostListener, Input, HostBinding, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appMoreBetterHighlight]'
+})
+export class MoreBetterHighlightDirective implements OnInit{
+
+  @Input() defaultColor:string = 'transparent';
+  @Input() highlightColor:string = 'red';
+
+  @HostBinding('style.backgroundColor') bgColor:string = '';
+
+  constructor() { 
+ 
+  }
+
+  ngOnInit(): void {
+    this.bgColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mousein(event: Event){
+    this.bgColor = this.highlightColor;
+  }
+  @HostListener('mouseleave') mouseout(event: Event){
+    this.bgColor = this.defaultColor;
+   
+  }
+
+
+}
